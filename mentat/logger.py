53832111521bb4848f5c5331119d5a6183a0766d
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2021-11-12 19:40:43 +0100

    missing file

---
+++ mentat/logger.py
@@ -0,0 +1,46 @@
+from .utils import *
+import logging
+
+class Logger():
+
+    def __init__(self, modname):
+        self.logger = logging.getLogger(__name__)
+
+    @public_method
+    def debug(self, message):
+        """
+        debug(message)
+
+        Print a debug message, prefixed with the object's name.
+
+        **Parameters**
+
+        - message: debug message
+        """
+        self.logger.debug("%s: %s" % (self.name, message))
+
+    @public_method
+    def info(self, message):
+        """
+        info(message)
+
+        Print a info message, prefixed with the object's name.
+
+        **Parameters**
+
+        - message: info message
+        """
+        self.logger.info("%s: %s" % (self.name, message))
+
+    @public_method
+    def error(self, message):
+        """
+        error(message)
+
+        Print an error message, prefixed with the object's name.
+
+        **Parameters**
+
+        - message: error message
+        """
+        self.logger.error("%s: %s" % (self.name, message))
