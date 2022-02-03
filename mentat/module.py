4520b89b93f6f429d4ec35297517dd2e6976812c
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2022-02-03 10:18:46 +0100

    clean: move module method decorator to utils

--- mentat/module.py
+++ mentat/module.py
@@ -3,42 +3,11 @@ import json
 import pathlib
 import re
 import logging
-import fnmatch
 
 from .utils import *
 from .parameter import Parameter, MetaParameter
 from .sequencer import Sequencer
 from .engine import Engine
-
-from functools import wraps
-def submodule_method(pattern_matching):
-    """
-    Decorator for Module methods that can be passed to submodules
-    by passing the submodule's name as first argument instead
-    of the usual first argument (ie parameter_name)
-    """
-    def decorate(method):
-        @wraps(method)
-        def decorated(self, *args, **kwargs):
-            name = args[0]
-            if pattern_matching and ('*' in name or '[' in name):
-                for n in fnmatch.filter(self.submodules.keys(), name):
-                    return [getattr(self.submodules[n], method.__name__)(*args[1:], **kwargs) for n in fnmatch.filter(self.submodules.keys(), name)]\
-                         + [getattr(self.submodules[self.aliases[n]], method.__name__)(*args[1:], **kwargs) for n in fnmatch.filter(self.aliases.keys(), name)]
-
-            elif name in self.submodules or name in self.aliases:
-
-                if name in self.aliases:
-                    name = self.aliases[name]
-
-                return getattr(self.submodules[name], method.__name__)(*args[1:], **kwargs)
-
-            else:
-
-                return method(self, *args, **kwargs)
-
-        return decorated
-    return decorate
 
 class Module(Sequencer):
     """
--- mentat/utils.py
+++ mentat/utils.py
@@ -1,3 +1,38 @@
+import fnmatch
+from functools import wraps
+
 def public_method(method):
+    """
+    Decorator for methods that should appear in the documentation.
+    """
     method._public_method = True
     return method
+
+def submodule_method(pattern_matching):
+    """
+    Decorator for Module methods that can be passed to submodules
+    by passing the submodule's name as first argument instead
+    of the usual first argument (ie parameter_name)
+    """
+    def decorate(method):
+        @wraps(method)
+        def decorated(self, *args, **kwargs):
+            name = args[0]
+            if pattern_matching and ('*' in name or '[' in name):
+                for n in fnmatch.filter(self.submodules.keys(), name):
+                    return [getattr(self.submodules[n], method.__name__)(*args[1:], **kwargs) for n in fnmatch.filter(self.submodules.keys(), name)]\
+                         + [getattr(self.submodules[self.aliases[n]], method.__name__)(*args[1:], **kwargs) for n in fnmatch.filter(self.aliases.keys(), name)]
+
+            elif name in self.submodules or name in self.aliases:
+
+                if name in self.aliases:
+                    name = self.aliases[name]
+
+                return getattr(self.submodules[name], method.__name__)(*args[1:], **kwargs)
+
+            else:
+
+                return method(self, *args, **kwargs)
+
+        return decorated
+    return decorate
